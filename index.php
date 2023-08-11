478e08f35e24f44160921db6c5ff19ee5572f864
AibiProject <zakizira0305@gmail.com>
2023-08-11 20:36:42 +0300

    menambahkan list

--- index.php
+++ index.php
@@ -8,5 +8,10 @@
 <body>
   <h1>remote</h1>
   <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nobis quaerat nihil voluptatum tempore repudiandae reprehenderit deleniti ut minima vero! Ad velit accusantium placeat obcaecati, culpa consequuntur expedita explicabo. Asperiores, assumenda!</p>
+  <ul>
+    <li>1</li>
+    <li>2</li>
+    <li>3</li>
+  </ul>
 </body>
 </html>
